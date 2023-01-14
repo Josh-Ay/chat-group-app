6156b9baa38a22e8f5539e0ca46eb35b092a711c
Ayoola <ayo.oloyede16@gmail.com>
2023-01-14 19:59:29 +0100

    Comment out 'calling' features

--- src/App.js
+++ src/App.js
@@ -5,30 +5,30 @@ import MainPage from './pages/MainPage/MainPage';
 import PageNotFound from './pages/PageNotFound/PageNotFound';
 import ProfilePage from './pages/ProfilePage/ProfilePage';
 import SettingsPage from './pages/SettingsPage/SettingsPage';
-import PeerCallPage from './features/Calling/pages/PeerCallPage';
-import GroupCallPage from './features/Calling/pages/GroupCallPage';
-import { socket } from './features/Calling/utils/socketInstance';
-import { useStreamContext } from './contexts/StreamContext';
-import { STREAM_REDUCER_ACTIONS } from './reducers/StreamReducer';
+// import PeerCallPage from './features/Calling/pages/PeerCallPage';
+// import GroupCallPage from './features/Calling/pages/GroupCallPage';
+// import { socket } from './features/Calling/utils/socketInstance';
+// import { useStreamContext } from './contexts/StreamContext';
+// import { STREAM_REDUCER_ACTIONS } from './reducers/StreamReducer';
 
 function App() {
   
-  const { dispatchToStreamState } = useStreamContext()
+  // const { dispatchToStreamState } = useStreamContext()
   
-  useEffect(() => {
+  // useEffect(() => {
 
-    socket.on("connect", () => {
-      dispatchToStreamState({ type:  STREAM_REDUCER_ACTIONS.UPDATE_SOCKET_ID, payload: { value: socket.id }});
-    })
+  //   socket.on("connect", () => {
+  //     dispatchToStreamState({ type:  STREAM_REDUCER_ACTIONS.UPDATE_SOCKET_ID, payload: { value: socket.id }});
+  //   })
 
-  }, [])
+  // }, [])
 
   return <Routes>
     <Route path='/' element={<MainPage />} />
     <Route path='/profile/:id' element={<ProfilePage />} />
     <Route path='/settings' element={<SettingsPage />} />
-    <Route path='/peer-call' element={<PeerCallPage />} />
-    <Route path='/group-call' element={<GroupCallPage />} />
+    {/* <Route path='/peer-call' element={<PeerCallPage />} /> */}
+    {/* <Route path='/group-call' element={<GroupCallPage />} /> */}
     <Route path='*' element={<PageNotFound />} />
   </Routes>
 }
